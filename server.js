7e1811c82ef047e35ae6a98f847de11b0f88f8a8
Abboudy Gogo <abboudygogo@outlook.com>
2020-07-22 10:22:58 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,46 +1,35 @@
-// importing packages module to server 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy; // strategies of login : email , FB , jwt,,,,,
-const ExtractJwt = require('passport-jwt').ExtractJwt; // extrcat jwt startegy specifically 
-const secret = 'Kjj40n2jkjdm5M'; // coming from the users route to be able to read the toekn contents 
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const secret = "s3cr3t100";
 
+const cors = require('cors');
 
-// importing routes
-const GroupsRoute = require('./routes/GroupsRoute.js');
-const EventsRoute = require('./routes/EventsRoute.js');
-//const FeedsRoute = require('./routes/FeedsRoute.js');
-//const UsersRoute = require('./routes/UsersRoute.js');
-//const EmailsRoute = require('./routes/EmailsRoute.js');
-//const UsersModel = require('./models/UsersModel.js'); // to find the user in the DB 
-const e = require('express');
+const UsersModel = require('./models/UsersModel');
 
-// function to pass to express to make sure the use is authenticated 
-
-//Options for passport
-const passportJwtOptins = {
+const passportJwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey : secret
-
+    secretOrKey: secret
 };
-// reads the content of the json token 
-const passportJwt = (passport) =>{
+
+const passportJwt = (passport) => {
     passport.use(
         new JwtStrategy(
-            passportJwtOptins, 
-            (jwtPayLoad, done)=>{
-                // extarct and find the user by their ID ( contained in jwt)
-                UsersModel.findOne({_id:jwtPayLoad.id})
+            passportJwtOptions,
+            (jwtPayload, done) => {
+
+                UsersModel.findOne({ _id: jwtPayload.id })
                 .then(
-                    (document)=>{
+                    (document) => {
                         return done(null, document);
                     }
-                ).catch(
-                    (err)=>{
-                        return done(null,null); 
+                )
+                .catch(
+                    (err) => {
+                        return done(null, null);
                     }
                 )
             }
@@ -48,106 +37,109 @@ const passportJwt = (passport) =>{
     )
 };
 
-//execute the express function - creating server object
+const GroupsRoute = require('./routes/GroupsRoute.js');
+const FeedsRoutes = require('./routes/FeedsRoutes');
+const UsersRoutes = require('./routes/UsersRoutes');
+const EventsRoute = require('./routes/EventsRoute.js');
+
 const server = express();
-// configure express to use the body-parser
-server.use(bodyParser.urlencoded({extended: false}));
+
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
-//
-server.use(cors());
-// configure express to use passport 
 server.use(passport.initialize());
-passportJwt(passport); // invoking the function - using the package name : passport
+server.use(cors());
 
-
-
+passportJwt(passport);
 
 const dBURL = 'mongodb+srv://admin01:db12345@cluster0.spafu.mongodb.net/finalproject?retryWrites=true&w=majority';
-mongoose.connect(dBURL, {
-    'useNewUrlParser' : true,
-    'useUnifiedTopology' : true
-}
+
+mongoose.connect(
+    dbURL,
+    {
+        'useNewUrlParser' : true,
+        'useUnifiedTopology': true
+    }
 ).then(
-    ()=> {
-        console.log("You are connected to the MongoDB!");
-    } 
+    ()=>{
+        console.log('You are connected MongoDB');
+    }
 ).catch(
-    (e)=> {
+    (e)=>{
         console.log('catch', e);
-    } 
+    }
 );
-// using the routes 
-server.use(
-    '/groups', 
-    // to make the route private - can't be accessed unliss logged in 
-    //passport.authenticate('jwt', {session:false}),
-    GroupsRoute
-);
-server.use(
-    '/events', 
-    // to make the route private - can't be accessed unliss logged in 
-    //passport.authenticate('jwt', {session:false}),
-    EventsRoute
-);
-/*server.use(
-    '/feeds', 
-    // to make the route private - can't be accessed unliss logged in (have a token) 
-    passport.authenticate('jwt', {session:false}),
-    FeedsRoute
-);
-server.use(
-    '/users', 
-    UsersRoute
-);
-server.use(
-    '/emails', 
-    EmailsRoute
-); */ 
-//creating route for landing page
-server.get(
-'/', 
-(req, res) => {
-res.send("<h1>Welcome to the website.<h1>" +
-"<a href=about>About </br> </a>" +
-"<a href=contacts>Contacts </br> </a>" +
-"<a href=products>Products </br> </a>" +
-"<a href=*>Location </br> </a>"
 
-); 
-} 
+server.use(
+    '/groups',
+    passport.authenticate('jwt', {session:false}), 
+    GroupsRoutes
+);
+
+server.use(
+    '/feeds',
+    // passport.authenticate('jwt', {session:false}), // Use passport-jwt to authenticate
+    FeedsRoutes
+);
+
+server.use(
+    '/users',
+    UsersRoutes
+);
+
+server.use(
+    '/events',
+    EventsRoutes
 );
 
 server.get(
-    '/about', 
+    '/',
     (req, res) => {
-    res.send("<h1>About Us<h1>"); 
-    } 
-    );
-
-server.get(
-'/contacts', 
-(req, res) => {
-res.send("<h1>Please contact this email for more details <h1>" + "<a href=email@hotmail.com>email@hotmail.com</a>"); 
-} 
+        res.send("<h1>Welcome to Communitalk/h1>" +
+        "<a href=about>About </br> </a>" +
+        "<a href=contacts>Contacts </br> </a>" +
+        "<a href=products>Products </br> </a>" +
+        "<a href=*>Location </br> </a>"
+        );
+    }
 );
 
 server.get(
-    '/groups', 
+    '/about',
     (req, res) => {
-    res.send("<h1>Mobile Phones </br> TVs </br> Tablets </br> Computers<h1>"); 
-    } 
-    );
+        res.send("<h1>About us!</h1>");
+    }
+);
 
 server.get(
-        '*', 
-        (req, res) => {
-        res.send("<h1>Error 404. Page Not Found!<h1>"); 
-        } 
-    );
+    '/Contactus',
+    (req, res) => {
+        res.send("<h1>Contact us!</h1>" +
+        "<a href='/'>home</a>"
+        );
+    }
+);
 
+server.get(
+    '/groups',
+    (req, res) => {
+        res.send("<h1>Find your community</h1>"+
+        "<a href='/'>home</a>"
+        );
+    }
+);
 
-// assign a port (range 3000 - 9000) to the server
-// http://localhost:8080 - 127.0.0.1 
-server.listen(8080, ()=> {
-    console.log("You are connected to the server!");
-} ); 
+  
+ server.get(
+    '*',
+    (req, res) => {
+        res.send("<h1>404 Page not found</h1>");
+    }
+);
+
+// Connect to port (range 3000 - 9999)
+// http://127.0.0.1:8080 (aka http://localhost:8080)
+server.listen( 
+    8080, ()=>{
+        console.log('You are connected');
+}
+);
